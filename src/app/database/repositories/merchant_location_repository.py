d435cc617f3d8d1af1b7c6e08804d2d44561ad8f
Hansel Loshaless <hansel.loshaless@cimbniaga.co.id>
2025-04-01 18:28:06 +0700

    add query to get all nearest merchant location by user id

--- src/app/database/repositories/merchant_location_repository.py
+++ src/app/database/repositories/merchant_location_repository.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import text
+from sqlalchemy import text, bindparam
 from typing import List
 
 from app.models.merchant_model import MerchantLocationModel
@@ -37,5 +37,50 @@ class MerchantLocationRepository:
                 "user_longitude": longitude,
                 "user_latitude": latitude,
                 "parameter_distance": max_distance
+            }
+        )
+
+    def get_distinct_nearby_merchant_locations_by_lat_long_and_user_id(self, latitude: float, longitude: float,
+                                                                       max_distance: float, user_id_list: List[int]):
+        query = text("""
+            WITH user_location AS (
+                SELECT
+                    ST_Transform(ST_SetSRID(ST_MakePoint(:user_longitude, :user_latitude), 4326), 3857) AS user_point
+            ),
+            distance_data AS (
+                SELECT
+                    u.full_name,
+                    rl.user_id,
+                    rl.name,
+                    rl.address,
+                    rl.latitude,
+                    rl.longitude,
+                    ST_Distance(
+                        ST_Transform(ST_SetSRID(ST_MakePoint(rl.longitude, rl.latitude), 4326), 3857),
+                        (SELECT user_point FROM user_location)
+                    ) AS distance_meters
+                FROM
+                    merchant_locations rl
+                JOIN "USER" u ON u.user_id = rl.user_id
+                CROSS JOIN user_location
+                WHERE
+                    rl.user_id IN :user_id_list  -- Note: no parentheses around the parameter
+                    AND ST_Distance(
+                        ST_Transform(ST_SetSRID(ST_MakePoint(rl.longitude, rl.latitude), 4326), 3857),
+                        (SELECT user_point FROM user_location)
+                    ) <= :parameter_distance
+            )
+            SELECT DISTINCT ON (user_id) *
+            FROM distance_data
+            ORDER BY user_id, distance_meters;
+        """).bindparams(bindparam('user_id_list', expanding=True))
+
+        return self.db.execute(
+            query,
+            {
+                "user_longitude": longitude,
+                "user_latitude": latitude,
+                "parameter_distance": max_distance,
+                "user_id_list": user_id_list
             }
         )
