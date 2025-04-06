3f15a994d3e4cd19405db595b42606011c67a0d0
Hansel Loshaless <hansel.loshaless@cimbniaga.co.id>
2025-04-06 10:06:21 +0700

    change persona input

--- src/app/schemas/user_preference_schema.py
+++ src/app/schemas/user_preference_schema.py
@@ -1,29 +1,73 @@
 # Pydantic models for request/response
-from datetime import datetime, date
-from typing import Optional, Dict, Any
+from datetime import date
+from typing import Dict, Any, List
 from pydantic import BaseModel
 import datetime
-
+from enum import Enum
 
 class TransactionLimitation(BaseModel):
     amount: int
     start_date: date
     end_date: date
-    
+
     model_config = {
         "json_encoders": {
             date: lambda v: v.isoformat()
         }
     }
 
-class PersonaModel(BaseModel):
-    goals: str
-    interest: str
-    limitation: TransactionLimitation
+class SavingHabit(str, Enum):
+    REGULAR = "rutin"
+    SELDOM = "jarang"
+    NEVER = "tidak pernah menabung"
+
+class FinancialBehavior(str, Enum):
+    CONSERVATIVE = "conservative"
+    BALANCED = "balanced"
+    RISK_TAKER = "risk taker"
+
+class UserPersonaProfile(BaseModel):
+    transaksi_limitation: TransactionLimitation
+    lifestyle: List[str]
+    financial_behavior: FinancialBehavior
+    financial_goals: List[str]
+    preferred_categories: List[str]
+    saving_habit: SavingHabit
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "transaksi_limitation": {
+                        "amount": 100000,
+                        "start_date": "2025-04-03",
+                        "end_date": "2025-07-03"
+                    },
+                    "lifestyle": [
+                        "Hemat",
+                        "Keluarga",
+                        "Traveler"
+                    ],
+                    "financial_behavior": "balanced",
+                    "financial_goals": [
+                        "Dana pensiun",
+                        "Beli rumah",
+                        "Dana darurat"
+                    ],
+                    "preferred_categories": [
+                        "Food & Beverages",
+                        "Groceries",
+                        "Investasi"
+                    ],
+                    "saving_habit": "rutin"
+                }
+            ]
+        }
+    }
 
 class UserPreferenceUpdate(BaseModel):
     full_name: str
-    persona: PersonaModel
+    persona: UserPersonaProfile
     pekerjaan: str
     usia: int
     marital_status: str
@@ -34,7 +78,7 @@ class UserPreferenceResponse(BaseModel):
     user_id: int
     preferences: Dict[str, Any]
     last_updated: datetime.datetime
-    persona: PersonaModel
+    persona: UserPersonaProfile
     pekerjaan: str
     usia: int
     marital_status: str
